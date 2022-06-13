f92d382d74bd4cad922d88429045b6b9c3f87747
fatcat-klm <105243410+fatcat-klm@users.noreply.github.com>
2022-06-13 17:11:09 +0300

    Add files via upload

--- final.py
+++ final.py
@@ -8,6 +8,7 @@ import numpy as np
 import seaborn as sns
 from geopy import distance
 from shapely.geometry import Point
+
 with st.echo(code_location='below'):
     matplotlib.use("Agg")
     st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -93,14 +94,7 @@ with st.echo(code_location='below'):
         "[Программа на основе](https://github.com/maladeep/palmerpenguins-streamlit-eda)")
 
 
-    @st.cache(persist=True, show_spinner=True)
-    def get_dist(rows):
-        data_url = (
-            "https://github.com/fatcat-klm/vsosh/raw/main/%D0%9A%D0%BE%D0%BF%D0%B8%D1%8F%20moscow%20schools%20-%20winners%20-%20moscow%20schools%20-%20winners%20(2)%20-%20moscow%20schools%20-%20winners%20-%20moscow%20schools%20-%20winners%20(2).csv.zip")
-        df = pd.read_csv(data_url, nrows=rows)
-        return df
-
-    df_new = get_dist(50000)
+    df_new = get_data(50000)
 
     def get_distance():
         distance_from_c = []
@@ -113,9 +107,10 @@ with st.echo(code_location='below'):
     punk = df_new['distance_from_center'].to_numpy()
     best_solution = np.mean(punk)
     st.write(best_solution)
-    
+
     def get_coords(lat, long):
         return Point(long, lat)
+    
     df_new['coords'] = df_new[['lat', 'long']].apply(lambda x: get_coords(*x), axis=1)
     m = folium.Map(location=[55.753544, 37.621211], zoom_start=10)
     FastMarkerCluster(data=[[lat, lon] for lat, lon in zip(df_new['lat'], df['long'])]).add_to(m)
