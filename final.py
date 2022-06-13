2fde8827a36b619073c7e123a0c75ecaafb5c008
fatcat-klm <105243410+fatcat-klm@users.noreply.github.com>
2022-06-13 11:43:31 +0300

    Add files via upload

--- final.py
+++ final.py
@@ -91,21 +91,4 @@ with st.echo(code_location='below'):
     st.sidebar.markdown(
         "[Программа на основе](https://github.com/maladeep/palmerpenguins-streamlit-eda)")
 
-    import pandas as pd
-    from geopy.geocoders import Nominatim
-    from geopy.extra.rate_limiter import RateLimiter
-
-    locat = df.query('df_ShortName')
-    df_new = pd.DataFrame({'add': locat})
-
-    # Creating an instance of Nominatim Class
-    geolocator = Nominatim(user_agent="my_request")
-
-    # applying the rate limiter wrapper
-    geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
-
-    # Applying the method to pandas DataFrame
-    df_new['location'] = df_new['add'].apply(geocode)
-    df_new['Lat'] = df_new['location'].apply(lambda x: x.latitude if x else None)
-    df_new['Lon'] = df_new['location'].apply(lambda x: x.longitude if x else None)
-    st.write(df_new)
+    
