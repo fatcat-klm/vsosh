d396418e0c02951c7858ae1aa93fdb9cbae6b517
Artem Makogon <artem.makogon.nsk@gmail.com>
2022-06-13 21:13:22 +0300

    1000

--- final.py
+++ final.py
@@ -26,7 +26,7 @@ with st.echo(code_location='below'):
         return df
 
 
-    df = get_data(1)
+    df = get_data(1000)
     st.sidebar.subheader('Описание параметров датасета')
     st.sidebar.subheader('Анализировать данные')
     st.markdown("### Что-то про  шоколад")
